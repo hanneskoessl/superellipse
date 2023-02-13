ee2074de67ea0643e2d249f168ed05d4943f8ec9
Hannes Kößl <125126437+hanneskoessl@users.noreply.github.com>
2023-02-13 15:26:36 +0100

    Update module_particles_test3_SE4_2.F90

--- module_particles_test3_SE4_2.F90
+++ module_particles_test3_SE4_2.F90
@@ -1,84 +1,3 @@
-!  LU decomposition form J-P Moreau (http://jean-pierre.moreau.pagesperso-orange.fr/Fortran/lu_f90.txt)
-!  ***************************************************************
-!  * Given an N x N matrix A, this routine replaces it by the LU *
-!  * decomposition of a rowwise permutation of itself. A and N   *
-!  * are input. INDX is an output vector which records the row   *
-!  * permutation effected by the partial pivoting; D is output   *
-!  * as -1 or 1, depending on whether the number of row inter-   *
-!  * changes was even or odd, respectively. This routine is used *
-!  * in combination with LUBKSB to solve linear equations or to  *
-!  * invert a matrix. Return code is 1, if matrix is singular.   *
-!  ***************************************************************
- Subroutine LUDCMP(A,N,INDX,D,CODE)
-        integer :: N, I, IMAX, J, K
-        double precision :: TINY=1.5D-16
-        double precision :: AMAX,DUM, SUM, VV(100)
-        double precision, dimension(8,8) :: A
-        integer CODE, D, INDX(N)
-
-        D=1; CODE=0
-
-        DO I=1,N
-            AMAX=0.d0
-            DO J=1,N
-                IF (DABS(A(I,J)).GT.AMAX) AMAX=DABS(A(I,J))
-            END DO ! j loop
-            IF(AMAX.LT.TINY) THEN
-                CODE = 1
-                RETURN
-            END IF
-            VV(I) = 1.d0 / AMAX
-            END DO ! i loop
-
-        DO J=1,N
-            DO I=1,J-1
-                SUM = A(I,J)
-                DO K=1,I-1
-                    SUM = SUM - A(I,K)*A(K,J) 
-                END DO ! k loop
-                A(I,J) = SUM
-            END DO ! i loop
-            AMAX = 0.d0
-            DO I=J,N
-                SUM = A(I,J)
-                DO K=1,J-1
-                    SUM = SUM - A(I,K)*A(K,J) 
-                END DO ! k loop
-                A(I,J) = SUM
-                DUM = VV(I)*DABS(SUM)
-                IF(DUM.GE.AMAX) THEN
-                    IMAX = I
-                    AMAX = DUM
-                END IF
-            END DO ! i loop  
-           
-            IF(J.NE.IMAX) THEN
-                DO K=1,N
-                    DUM = A(IMAX,K)
-                    A(IMAX,K) = A(J,K)
-                    A(J,K) = DUM
-                END DO ! k loop
-                D = -D
-                VV(IMAX) = VV(J)
-            END IF
-
-            INDX(J) = IMAX
-            IF(DABS(A(J,J)) < TINY) A(J,J) = TINY
-
-            IF(J.NE.N) THEN
-                DUM = 1.d0 / A(J,J)
-                DO I=J+1,N
-                    A(I,J) = A(I,J)*DUM
-                END DO ! i loop
-            END IF 
-        END DO ! j loop
-
-        RETURN
-        END subroutine LUDCMP
-
-
-
-
 program module_particles_test
 implicit none
     double precision :: RXIJ, RYIJ, PHII, PHIJ
@@ -97,10 +16,7 @@ implicit none
     double precision :: Det
 
 
-    ovrlap = .false.
-   
-    ! *** Using Overlap Criterion by Vielliard-Baron *** !
-    ! ** from: Phase Transitions of the Classical Hard-Ellipse System - The Journal of Chemical Physics, 1972, Vol 56, Issue 10 ** !                   
+    ovrlap = .false.               
 
     pi=4.*atan(1.d0)
     RXIJ = 0.81469565472048355
@@ -259,7 +175,7 @@ implicit none
             mat(2,7) = 0.
             mat(3,8) = 0. 
 
-            call ludcmp(mat,N,INDX,D,CODE)
+            call ludcmp(mat,N,INDX,D,CODE) ! LU decomposition (see http://jean-pierre.moreau.pagesperso-orange.fr/Fortran/lu_f90.txt)
             if (D == 1) then 
                 Det=1.
             else if ( D  == -1 ) then 
@@ -285,11 +201,3 @@ implicit none
     write(*,*) ovrlap
 
 end
-
-!            if ( (psi .gt. 0.0d0) .and. ((f1 .lt. 0.0d0) .or. (f2 .lt. 0.0d0)) ) then
-!             if ( (psi .gt. dble_epsilon_2) .and. ((f1 .lt. -dble_epsilon_2) .or. (f2 .lt. -dble_epsilon_2)) ) then
-!             if ( (abs(psi) .lt. dble_epsilon .or. psi .lt. 0.0d0) .and. ((abs(f1) .lt. dble_epsilon .or. f1 .lt. 0.0d0) .or. (abs(f2) .lt. dble_epsilon_2 .or. f2 .lt. 0.0d0)) ) then
-!                ovrlap = .false.
-!            else
-!                ovrlap = .true.
-!            end if
